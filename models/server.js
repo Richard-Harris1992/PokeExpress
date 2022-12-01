f4b3c9dd9fdcf19dea8ff94a4124d0fca4a621bd
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-12-01 13:25:22 -0600

    index.jsx view rendered at pokemon route

--- models/server.js
+++ models/server.js
@@ -11,12 +11,15 @@ const port = 3000;
 //Edit
 //Show
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 });
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon);
+    res.render('Index')
 });
 
 app.listen(port, () => console.log('3000'))
