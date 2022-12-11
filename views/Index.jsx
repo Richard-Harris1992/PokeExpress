2599da3de228cc94c751aaeacf352ecd9f8b834a
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2022-12-11 11:38:55 -0600

    deleted more uneeded coded

--- views/Index.jsx
+++ views/Index.jsx
@@ -14,7 +14,6 @@ const style = {
 
 class Index extends React.Component {
     render() {
-        console.log(this.props);
         return (
         <div> 
             <h1 style={style}> See All The Pokemon! </h1>
